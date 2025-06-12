b4289b7f91e036b7bac1ed0a523a725f5954f574
Dila Alpay <150535140+dila0704@users.noreply.github.com>
2025-06-12 13:17:23 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace kargo_takip_sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textTakipNo = new System.Windows.Forms.TextBox();
+            this.textAlici = new System.Windows.Forms.TextBox();
+            this.cmbTip = new System.Windows.Forms.ComboBox();
+            this.cmbDurum = new System.Windows.Forms.ComboBox();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.btnDurumGuncelle = new System.Windows.Forms.Button();
+            this.bnSorgula = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textTakipNo
+            // 
+            this.textTakipNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textTakipNo.Location = new System.Drawing.Point(103, 27);
+            this.textTakipNo.Name = "textTakipNo";
+            this.textTakipNo.Size = new System.Drawing.Size(167, 26);
+            this.textTakipNo.TabIndex = 0;
+            this.textTakipNo.TextChanged += new System.EventHandler(this.textTakipNo_TextChanged);
+            // 
+            // textAlici
+            // 
+            this.textAlici.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textAlici.Location = new System.Drawing.Point(103, 59);
+            this.textAlici.Name = "textAlici";
+            this.textAlici.Size = new System.Drawing.Size(167, 26);
+            this.textAlici.TabIndex = 1;
+            this.textAlici.TextChanged += new System.EventHandler(this.textAlici_TextChanged);
+            // 
+            // cmbTip
+            // 
+            this.cmbTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbTip.FormattingEnabled = true;
+            this.cmbTip.Location = new System.Drawing.Point(161, 66);
+            this.cmbTip.Name = "cmbTip";
+            this.cmbTip.Size = new System.Drawing.Size(121, 28);
+            this.cmbTip.TabIndex = 2;
+            this.cmbTip.Text = "Seçiniz";
+            this.cmbTip.SelectedIndexChanged += new System.EventHandler(this.cmbTip_SelectedIndexChanged);
+            // 
+            // cmbDurum
+            // 
+            this.cmbDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbDurum.FormattingEnabled = true;
+            this.cmbDurum.Location = new System.Drawing.Point(161, 31);
+            this.cmbDurum.Name = "cmbDurum";
+            this.cmbDurum.Size = new System.Drawing.Size(121, 28);
+            this.cmbDurum.TabIndex = 3;
+            this.cmbDurum.Text = "Seçiniz";
+            this.cmbDurum.SelectedIndexChanged += new System.EventHandler(this.cmbDurum_SelectedIndexChanged);
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnOlustur.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnOlustur.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOlustur.ForeColor = System.Drawing.Color.White;
+            this.btnOlustur.Location = new System.Drawing.Point(81, 270);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(190, 35);
+            this.btnOlustur.TabIndex = 4;
+            this.btnOlustur.Text = "Gönderi Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = false;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // btnDurumGuncelle
+            // 
+            this.btnDurumGuncelle.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnDurumGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnDurumGuncelle.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDurumGuncelle.ForeColor = System.Drawing.Color.White;
+            this.btnDurumGuncelle.Location = new System.Drawing.Point(81, 325);
+            this.btnDurumGuncelle.Name = "btnDurumGuncelle";
+            this.btnDurumGuncelle.Size = new System.Drawing.Size(190, 37);
+            this.btnDurumGuncelle.TabIndex = 5;
+            this.btnDurumGuncelle.Text = "Durum Güncelle";
+            this.btnDurumGuncelle.UseVisualStyleBackColor = false;
+            this.btnDurumGuncelle.Click += new System.EventHandler(this.btnDurumGuncelle_Click);
+            // 
+            // bnSorgula
+            // 
+            this.bnSorgula.BackColor = System.Drawing.Color.MediumPurple;
+            this.bnSorgula.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bnSorgula.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bnSorgula.ForeColor = System.Drawing.Color.White;
+            this.bnSorgula.Location = new System.Drawing.Point(81, 385);
+            this.bnSorgula.Name = "bnSorgula";
+            this.bnSorgula.Size = new System.Drawing.Size(190, 33);
+            this.bnSorgula.TabIndex = 7;
+            this.bnSorgula.Text = "Sorgula";
+            this.bnSorgula.UseVisualStyleBackColor = false;
+            this.bnSorgula.Click += new System.EventHandler(this.bnSorgula_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(6, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 22);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Takip No:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(6, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Alıcı:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(6, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 22);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Göneri Durumu: ";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(6, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 22);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Gönderi Tipi:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(313, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(469, 462);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textTakipNo);
+            this.groupBox1.Controls.Add(this.textAlici);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(291, 105);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kişisel Bilgiler";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.cmbDurum);
+            this.groupBox2.Controls.Add(this.cmbTip);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(16, 139);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(291, 116);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gönderi Bilgileri";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(758, 452);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bnSorgula);
+            this.Controls.Add(this.btnDurumGuncelle);
+            this.Controls.Add(this.btnOlustur);
+            this.Name = "Form1";
+            this.Text = "KargoTakip";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textTakipNo;
+        private System.Windows.Forms.TextBox textAlici;
+        private System.Windows.Forms.ComboBox cmbTip;
+        private System.Windows.Forms.ComboBox cmbDurum;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.Button btnDurumGuncelle;
+        private System.Windows.Forms.Button bnSorgula;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
